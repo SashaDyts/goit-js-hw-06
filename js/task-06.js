6bf0b4de76983dbda9d04bb52ffbae871c030373
SashaDyts <lentrofan@gmail.com>
2022-05-03 16:35:50 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,11 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputElBlur);
 
 function onInputElBlur(event) {
-  if (event.currentTarget.value.length < inputEl.dataset.length) {
-    event.currentTarget.classList.remove('valid');
-    event.currentTarget.classList.add('invalid');
-  } else {
+  if (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
     event.currentTarget.classList.remove('invalid');
     event.currentTarget.classList.add('valid');
+  } else {
+    event.currentTarget.classList.remove('valid');
+    event.currentTarget.classList.add('invalid');
   }
 }
